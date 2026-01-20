69c77828d5e144911edf5c2bc9593172e1ac6ef9
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-20 18:00:59 -0300

    Resposta

--- PythonProject/ex036.py
+++ PythonProject/ex036.py
@@ -9,3 +9,16 @@ elif prestaçao > salario * 0.3:
 
 print('Para pagar um casa de R${} em {} anos a prestação será de R${:.2f}'.format(casa, ano, prestaçao, ))
 
+#professor guanabara
+
+casa = float(input('Qual o valor da casa? '))
+salário = float(input('Salário do comprador: R$'))
+anos = float(input('Quantos anos de financiamento? '))
+prestação = casa / (anos * 12)
+mínimo = salário * 0.30
+print('Para pagar um casa de R${:.2f} em {} anos'.format(casa, anos,),end='')
+print(' a pretação será de R${:.2}'.format(prestação))
+if prestação <= mínimo:
+    print('Seu financiamento pode ser aprovado!')
+else:
+    print('Empréstimo negado!')
