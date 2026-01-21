dee64c158b203530cf1ab1804799ce86e9179796
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-21 18:27:18 -0300

    ex044 professor guanabara

--- PythonProject/ex044.py
+++ PythonProject/ex044.py
@@ -14,4 +14,29 @@ elif num == 3:
 elif num == 4:
     print('Em 3x ou mais no cartão o proço ficará em {:.2f}'.format(n1 + (n1 * 0.20)))
 
+#professor guanabara
+print('{:=^40}'.format('LOJAS GUANABARA'))
+preço = float(input('Digite o valor das compras: R$'))
+print('''FORMAS DE PAGAMENTO
+[1] À VISTA/CHEQUE
+[2] À VISTA CARTÃO
+[3] 2X NO CARTÃO
+[4] 3X OU MASI NO CATÃO''')
+opçao = int(input('Qual é a opção? '))
+if opçao == 1:
+    total = preço - (preço * 10 / 100)
+elif opçao == 2:
+    total = preço - (preço * 5 / 100)
+elif opçao == 3:
+    total = preço
+    parcela = total / 2
+    print('Sua compra será parcelada em 2x de R${:.2f}'.format(parcela))
+elif opçao == 4:
+    total = preço + (preço * 20 / 100)
+    totalparc = int(input('Quantas parcelas? '))
+    parcela = total / totalparc
+    print('Sua compra será parcelada em {}x de R${:.2f} com juros'.format(totalparc, parcela))
+
+print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(preço, total))
+
 
