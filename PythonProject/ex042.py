c3f2773c0f57c5c8d833f12aa26d52dca4975a70
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-21 13:33:33 -0300

    ex042_ajustes

--- PythonProject/ex042.py
+++ PythonProject/ex042.py
@@ -4,12 +4,16 @@ o = float(input('Digite o valor do outro lado:'))
 
 if  n < m + o and m < n + o and o < m + n:
     print('É um trângulo')
-if n == m and m == o:
-    print('Todos os lados são iguais.')
-    print('É um triângulo equilatero')
-elif n == m and o != m:
-    print('Dois lados são iguais e um diferente')
-    print('É um triângulo isósceles')
-elif n != m and m != o and o != n:
-    print('Três lados diferentes')
-    print('Você tem um triagulo Escaleno')
+    if n == m and m == o:
+        print('Todos os lados são iguais.')
+        print('É um triângulo equilatero')
+    if n == m and o != m:
+        print('Dois lados são iguais e um diferente')
+        print('É um triângulo isósceles')
+    elif n != m and m != o and o != n:
+        print('Três lados diferentes')
+        print('Você tem um triagulo Escaleno')
+else:
+    print('Não foi possível formar um triângulo')
+
+
