6861ab498d4b7d7521942993a64030b8306a7a60
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-21 10:44:09 -0300

    ex041

---
+++ PythonProject/ex041.py
@@ -0,0 +1,15 @@
+print('-=-\033[1;30m'*30)
+print('Bem Vindo a Confederação de Natação de Jundiai caro Estudante ! '.center(100))
+print('\033[1;30m-=-\033[m'*30)
+print('\n')
+
+idade = int(input('\033[1;36mDigite a sua idade para ver em qual categoria você se enquadra:\033[m '))
+
+if idade <= 9:
+    print('Categoria: \033[1;32mMIRIM\033[m ')
+elif idade <= 14:
+    print('Categoria: \033[1;34mINFANTIL\033[m')
+elif idade <= 25:
+    print('Categoria: \033[1;35mSENIOR\033[m')
+elif idade > 25:
+    print('Categoria: \033[1;31mMASTER\033[m')
