dc87b204cccc3aa9a0e66176ec3f8748da77aeec
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-21 10:32:40 -0300

    ex040 professor guanabara

--- PythonProject/ex040.py
+++ PythonProject/ex040.py
@@ -11,3 +11,17 @@ elif media >= 5 and media < 7:
     print('Recuperação')
 elif media < 5:
     print('Reprovado')
+
+#professor guanabara
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+média = (nota1 + nota2) / 2
+print('Tirando {:.1f} e {:.1f}, a média do aluno é {:.1f}'.format(nota1,nota2, média))
+if 7 > média >= 5:
+    print('O aluno está em recuperação')
+elif média < 5:
+    print('O aluno está REPRVADO.')
+elif média <= 7:
+    print('O aluno está reprovado')
+
