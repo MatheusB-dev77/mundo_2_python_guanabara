075d6c5e0c9e9a74dfa7706bedf258fb4e227755
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-22 19:23:40 -0300

    ex049 professor guanabara

--- PythonProject/ex049.py
+++ PythonProject/ex049.py
@@ -1,3 +1,9 @@
 n = int(input('Digite um numero para saber sua tabuada:'))
 for c in range(0, 11):
     print('{} x {} = {}'.format(n, c, c * n))
+
+#professor guanabara
+
+num = int(input('digite um número para ver sua tabuada:'))
+for c in range(1,11):
+    print('{} x {} = {:.2f}'.format(num, c, c * num))
