f25e5e117910ff0c82429faf3593b1149dbef17d
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-22 18:09:51 -0300

    ex048 professor guanabara

--- PythonProject/ex48.py
+++ PythonProject/ex48.py
@@ -6,3 +6,11 @@ for c in range(1, 501):
 print()
 print('FIM')
 
+#professor guanabara
+soma = 0 #soma o valores entre si
+cont = 0 #conta quantos valores tem
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        soma = soma + c
+        cont = cont + 1
+print('A soma de todos os {} valores solicitados é {}'.format(cont, soma))
