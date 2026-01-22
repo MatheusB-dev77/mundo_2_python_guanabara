c368cdd50b78eaf163f6a24ba9c44366949f7e31
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-22 16:20:46 -0300

    ex046 professor guanabara

--- PythonProject/ex046.py
+++ PythonProject/ex046.py
@@ -9,3 +9,10 @@ for i in range(10, -1, -1):
     print(i)
 
 print('Bommm')
+
+#professor guanabara
+from time import sleep
+for cont in range(10, -1, -1):
+    print(cont)
+    sleep(1)
+print('BOMM! BUM!')
