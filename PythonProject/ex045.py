167c5c324aa77144cd58ec88742867e557d4e12b
Matheus Batista <aluiziomatheus@gmail.com>
2026-01-21 22:46:47 -0300

    ex045 professor guanabara

--- PythonProject/ex045.py
+++ PythonProject/ex045.py
@@ -32,4 +32,52 @@ elif sorteador == 'Tesoura':
         print('EMPATE')
 
 
+#professor guanabara
+from random import randint
+from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+print('''Suas opções
+[ 0 ] Pedra
+[ 1 ] Papel
+[ 2 ] Tesoura''')
+jogador = int(input('Qual é a sua jogada?'))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO!!!')
+print('\n')
+print('O computador escolheu {}'.format(itens[computador]))
+print('Jogador jogou {}'.format(itens[jogador]))
+print('\n')
+if computador == 0:
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('Jogador Venceu!')
+    elif jogador == 2:
+        print('Computador Venceu!')
+    else:
+        print('Jogada invalida!')
+elif computador == 1:
+    if jogador == 0:
+        print('Computador VENCEU!')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('Jogador VECEU!')
+    else:
+        print('Jogada invalida!')
+elif computador == 2:
+    if jogador == 0:
+        print('Jogador VENCEU!')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('Computador VENCEU!')
+    else:
+        print('Jogada invalida!')
+
+
 
