1316e469e652460c50d57e50d939bb7cb5478e63
MatheusB-dev77 <aluiziomatheus@gmail.com>
2026-01-29 22:42:31 -0300

    ex54 professor guanabara

--- PythonProject/ex054.py
+++ PythonProject/ex054.py
@@ -16,3 +16,17 @@ for c in range(1, 7):
 #professor gunabara
 
 
+atual = date.today().year
+totmaior = 0
+totmenor = 0
+for pess in range (1,8):
+    nasc = int(input('Em que ano a {}ª pessoa nasceu'.format(pess)))
+    idade = atual - nasc
+    if idade >= 21:
+        totmaior += 1
+    else:
+        totmenor += 1
+print('Ao todo tivemos {} pessoas maiores de idade'.format(totmaior))
+print('E também tivemo {} pessoal menores de idade'.format(totmenor))
+
+
